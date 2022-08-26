5d0455f805848637ae479252463e864ab71878f9
Arthur-Rosa <arthur.corporativo@outlook.com>
2022-08-26 18:40:10 -0300

    Click funcionando !

--- Login_Davi_Help/index.js
+++ Login_Davi_Help/index.js
@@ -1,16 +1,18 @@
 var btnLogin = document.getElementById('do-login');
-var idLogin = document.getElementById('login');
+var fundoSection = document.getElementById('fundo');
 
 var forgot = document.getElementById('do-forgot')
 var idforgot = document.getElementById('forgot');
 
 var username = document.getElementById('username');
 
-btnLogin.onclick = function(){
-  idLogin.innerHTML = '<p> Estamos felizes em vê-lo novamente </p><h1>' +username.value+ '</h1>';
-}
-forgot.onclick = function(){
-  idforgot.innerHTML = '<p> Estamos felizes em vê-lo novamente </p><h1>' +username.value+ '</h1>';
-}
+btnLogin.addEventListener('click',function () {
+  fundoSection.innerHTML = '<p> Estamos felizes em vê-lo novamente </p><br><a href="index.html">Voltar</a><h1>' +username.value+ '</h1>';
+})
+
+forgot.addEventListener('click',function () {
+  fundoSection.innerHTML = '<p> Esqueceu a senha ? </p><br><a href="index.html">Voltar</a><h1>' +username.value+ '</h1>';
+})  
+  
 
 //https://pt.piliapp.com/emoticon/  ¯\(°_o)/¯
